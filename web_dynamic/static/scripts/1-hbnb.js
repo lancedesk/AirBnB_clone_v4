e1398f9ca92f5e042577ce26954d6d0911d0a0fa
Lance Desk <lncedesk@gmail.com>
2024-03-01 08:03:30 -0800

    Make filters dynamic by updating 0-hbnb to 1-hbnb and creating 1-hbnb.html, 1-hbnb.js and scripts folder

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  console.log('entre');
+  
+  const amenities = {};
+  const $amenitiesHeader = $('.amenities H4');
+
+  $('input[type="checkbox"]').click(function () {
+    const $checkbox = $(this);
+    const id = $checkbox.attr('data-id');
+    const name = $checkbox.attr('data-name');
+    
+    if ($checkbox.prop('checked')) {
+      amenities[id] = name;
+    } else {
+      delete amenities[id];
+    }
+    
+    updateAmenitiesHeader();
+  });
+
+  function updateAmenitiesHeader() {
+    $amenitiesHeader.text(Object.values(amenities).join(', '));
+  }
+});
